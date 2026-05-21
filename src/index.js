dbdc86476fb6f6d6b4114c90be8573fb115aeada
pirlo47 <ntoahaep108@gmail.com>
2026-05-21 13:24:39 +0200

    Add MongoDB connection and body-parser setup in index.js; create ContactSchema in crmModel.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import routes from "./routes/crmRoutes";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000; 
+
+//mongodb connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb');
+
+//bodyparser setup 
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.json()); 
 
 routes(app); 
 
---
+++ src/models/crmModel.js
@@ -0,0 +1,33 @@
+//schema defination
+import mongoose from "mongoose";
+
+//create a schema variable 
+const Schema = mongoose.Schema;
+
+
+export const ContactSchema = new Schema({
+    firstName: {
+        type: String, 
+        require: "Enter a first name"
+    }, 
+    lastName: {
+        type: String, 
+        require: "Enter a last name"
+    }, 
+    email: {
+        type: String, 
+        required: true
+    }, 
+    company: {
+        type: String, 
+    }, 
+    phone: {
+        type: Number, 
+        require: true
+    }, 
+    created_date: {
+        type: Date, 
+        default: Date.now
+    }, 
+
+});
