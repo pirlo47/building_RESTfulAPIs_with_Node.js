3e17b7622018e33ee16fc1048823746178087973
pirlo47 <ntoahaep108@gmail.com>
2026-06-03 10:02:02 +0200

    Create UserModel to define users

---
+++ src/models/userModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+export const UserSchema = new Schema ({
+    userName : {
+        type: String, 
+        require: true
+    },
+    email: {
+        type: String, 
+        // required: true
+    }, 
+
+    hashPassword: {
+        type: String, 
+        required: true
+    }, 
+
+    created_date: {
+        type: Date, 
+        default: Date.now
+    }, 
+})
